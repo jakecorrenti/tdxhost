91634749a3179421b343aa15ebfd3bbea904f3db
Jake Correnti <jakecorrenti+github@proton.me>
2024-07-10 16:20:53 -0400

    Add shell completions
    Generate shell completions in the build script
    
    Signed-off-by: Jake Correnti <jakecorrenti+github@proton.me>

--- build.rs
+++ build.rs
@@ -3,8 +3,33 @@
 use clap::{Command, CommandFactory};
 use std::{io::Error, path::Path};
 
+use clap_complete::{generate, shells};
+
 #[path = "src/cli.rs"]
 mod cli;
+
+fn generate_shell_completion(cmd: &mut Command, out_dir: &Path) -> Result<(), Error> {
+    let mut buffer: Vec<u8> = Default::default();
+    generate(shells::Zsh, cmd, "tdxhost", &mut buffer);
+    std::fs::write(out_dir.join("tdxhost-completion-zsh.1"), &buffer)?;
+    buffer.clear();
+
+    generate(shells::Bash, cmd, "tdxhost", &mut buffer);
+    std::fs::write(out_dir.join("tdxhost-completion-bash.1"), &buffer)?;
+    buffer.clear();
+
+    generate(shells::Fish, cmd, "tdxhost", &mut buffer);
+    std::fs::write(out_dir.join("tdxhost-completion-fish.1"), &buffer)?;
+    buffer.clear();
+
+    generate(shells::PowerShell, cmd, "tdxhost", &mut buffer);
+    std::fs::write(out_dir.join("tdxhost-completion-powershell.1"), &buffer)?;
+    buffer.clear();
+
+    generate(shells::Elvish, cmd, "tdxhost", &mut buffer);
+    std::fs::write(out_dir.join("tdxhost-completion-elvish.1"), buffer)?;
+    Ok(())
+}
 
 fn generate_manpages(cmd: &Command, out_dir: &Path) -> Result<(), Error> {
     let man = clap_mangen::Man::new(cmd.clone());
@@ -16,12 +41,12 @@ fn generate_manpages(cmd: &Command, out_dir: &Path) -> Result<(), Error> {
 }
 
 fn main() -> std::io::Result<()> {
-    let out_dir = std::path::PathBuf::from(
-        std::env::var_os("OUT_DIR").ok_or_else(|| std::io::ErrorKind::NotFound)?,
-    );
-    let cmd = cli::Cli::command();
+    let out_dir =
+        std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?);
+    let mut cmd = cli::Cli::command();
 
     generate_manpages(&cmd, &out_dir)?;
+    generate_shell_completion(&mut cmd, &out_dir)?;
 
     Ok(())
 }
